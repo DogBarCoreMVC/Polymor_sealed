a7ef0eb94a232b75f2b97df0bfb453d84e6bac08
GitDogBar <Dogbar>
2021-08-15 17:10:21 +0700

    อธิบายเพิ่มเติม

--- Animal.cs
+++ Animal.cs
@@ -6,15 +6,20 @@ using System.Threading.Tasks;
 
 namespace Polymor_sealed
 {
-    public class Animal
+    public sealed class Animal//ถ้าใส่ key word "sealed" ตั้งแต่ใน class จะไม่สามารถทำการทืบทอดคลาสได้เลย 
     {
-        public virtual void Goes ()
+        /*public virtual void Goes ()//และ method นี้จะไม่ทำงานเพราะว่ามี key word "virtual" อยู่ แต่ถ้าต้องการจะให้ method นี้ทำงานให้ลบ key word "virtual" ออกไป
+        {
+            Console.WriteLine("Goes Class Animal");
+        }*///ตัวอย่าง
+
+        public void Goes()
         {
             Console.WriteLine("Goes Class Animal");
         }
     }
 
-    public class Dog : Animal
+    public class Dog : Animal//คลาสนี้จะไม่ทำงานเพราะว่า key word "sealed" ที่อยู่ใน class Animal หรือ คลาสแม่
     {
         public override void Goes()
         {
